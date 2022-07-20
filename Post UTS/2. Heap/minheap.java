ab0168fff7941fb419cc4aa1fd0fbf23f2b1d55d
Aditya Kesuma <aditezo24@gmail.com>
2022-07-20 11:15:57 +0700

    Max-Min Heap

---
+++ Post UTS/2. Heap/minheap.java
@@ -0,0 +1,125 @@
+public class minheap {
+    private int[] Heap;
+    private int size;
+    private int maxsize;
+    private static final int FRONT = 1;
+
+    public minheap(int maxsize) {
+
+        // This keyword refers to current object itself
+        this.maxsize = maxsize;
+        this.size = 0;
+
+        Heap = new int[this.maxsize + 1];
+        Heap[0] = Integer.MIN_VALUE;
+    }
+
+    private int parent(int pos) {
+        return pos / 2;
+    }
+
+    private int leftChild(int pos) {
+        return (2 * pos);
+    }
+
+    private int rightChild(int pos) {
+        return (2 * pos) + 1;
+    }
+
+    private boolean isLeaf(int pos) {
+
+        if (pos > (size / 2) && pos <= size) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void swap(int fpos, int spos) {
+
+        int tmp;
+        tmp = Heap[fpos];
+
+        Heap[fpos] = Heap[spos];
+        Heap[spos] = tmp;
+    }
+
+    private void minHeapify(int pos) {
+
+        // If the node is a non-leaf node and greater
+        // than any of its child
+        if (!isLeaf(pos)) {
+            if (Heap[pos] > Heap[leftChild(pos)]
+                    || Heap[pos] > Heap[rightChild(pos)]) {
+
+                // Swap with the left child and heapify
+                // the left child
+                if (Heap[leftChild(pos)] < Heap[rightChild(pos)]) {
+                    swap(pos, leftChild(pos));
+                    minHeapify(leftChild(pos));
+                }
+
+                // Swap with the right child and heapify
+                // the right child
+                else {
+                    swap(pos, rightChild(pos));
+                    minHeapify(rightChild(pos));
+                }
+            }
+        }
+    }
+
+    public void insert(int element) {
+
+        if (size >= maxsize) {
+            return;
+        }
+
+        Heap[++size] = element;
+        int current = size;
+
+        while (Heap[current] < Heap[parent(current)]) {
+            swap(current, parent(current));
+            current = parent(current);
+        }
+    }
+
+    public void print() {
+        for (int i = 1; i <= size / 2; i++) {
+
+            // Printing the parent and both childrens
+            System.out.print(
+                    " PARENT : " + Heap[i]
+                            + " LEFT CHILD : " + Heap[2 * i]
+                            + " RIGHT CHILD :" + Heap[2 * i + 1]);
+
+            // By here new line is required
+            System.out.println();
+        }
+    }
+
+    public int remove() {
+
+        int popped = Heap[FRONT];
+        Heap[FRONT] = Heap[size--];
+        minHeapify(FRONT);
+
+        return popped;
+    }
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("The Min Heap is ");
+
+        minheap minHeap = new minheap(7);
+        minHeap.insert(1);
+        minHeap.insert(10);
+        minHeap.insert(16);
+        minHeap.insert(19);
+        minHeap.insert(3);
+        minHeap.insert(5);
+        minHeap.print();
+        System.out.println("The min val is " + minHeap.remove());
+    }
+
+}
